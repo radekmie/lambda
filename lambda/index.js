658ecee93452f91c14a1858f2eca6b8639b3eeba
Radosław Miernik <radekmie@gmail.com>
2019-04-11 00:04:52 +0200

    Updated version.

--- lambda/index.js
+++ lambda/index.js
@@ -1,3 +1,31 @@
+const _wrapIf = (condition, string) => condition ? `(${string})` : string;
+const _found = (tokens, vars) => Var(vars.lastIndexOf(tokens.shift()));
+const _build = (tokens, vars, app) => !/^[a-z\(]$/.test(tokens[0]) ? app : _build(tokens, vars, App(app, tokens[0] === '(' ? _parse(tokens, vars, true) : _found(tokens, vars)));
+const _parse = (tokens, vars, bound) => {
+  switch (tokens[0]) {
+    case undefined:
+      throw new Error('ParsingError');
+
+    case '(':
+      tokens.shift(); // (
+      let app = _parse(tokens, vars);
+      while (tokens[0] !== ')')
+        app = App(app, _parse(tokens, vars));
+      tokens.shift(); // )
+      return bound ? app : _build(tokens, vars, app);
+
+    case 'λ':
+      tokens.shift(); // λ
+      const name = tokens.shift();
+      const bind = vars.length;
+      tokens.shift(); // .
+      return Lam(bind, _parse(tokens, vars.concat(name)));
+
+    default:
+      return _build(tokens, vars, _found(tokens, vars));
+  }
+};
+
 const App = (m, n) => ['App', m, n];
 const Lam = (x, n) => ['Lam', x, n];
 const Var = x => ['Var', x];
@@ -8,71 +36,25 @@ const isVar = term => term[0] === 'Var';
 
 const fold = (Var, Lam, App) => (term, ...args) => ({App, Lam, Var}[term[0]])(term, ...args);
 
-const fromString = source => {
-  source = source.replace(/\\/g, 'λ');
+const copy = fold(
+  M => Var(M[1]),
+  M => Lam(M[1], copy(M[2])),
+  M => App(copy(M[1]), copy(M[2]))
+);
 
-  let index = 0;
-  function parse(vars, bound) {
-    if (index > source.length)
-      throw Error(`Error while parsing ${source}`);
-
-    if (source[index] === '(') {
-      ++index; // (
-      let app = parse(vars);
-      while (source[index] !== ')')
-        app = App(app, parse(vars));
-      ++index; // )
-
-      if (!bound) {
-        while (/^[a-z\(]$/.test(source[index])) {
-          if (source[index] === '(')
-            app = App(app, parse(vars));
-          else
-            app = App(app, Var(vars.lastIndexOf(source[index++])));
-        }
-      }
-
-      return app;
-    }
-
-    if (source[index] === 'λ') {
-      ++index; // λ
-      const name = source[index++];
-      const bind = vars.length;
-      ++index; // .
-
-      return Lam(bind, parse(vars.concat(name)));
-    }
-
-    let app = Var(vars.lastIndexOf(source[index++]));
-    while (/^[a-z\(]$/.test(source[index])) {
-      if (source[index] === '(')
-        app = App(app, parse(vars, true));
-      else
-        app = App(app, Var(vars.lastIndexOf(source[index++])));
-    }
-
-    return app;
-  }
-
-  return parse([]);
-};
-
-const toName = x => 'xyz'[x];
+const toName = x => (x + 10).toString(36);
 const toAST = fold(
   M => `Var(${M[1]})`,
   M => `Lam(${M[1]}, ${toAST(M[2])})`,
   M => `App(${toAST(M[1])}, ${toAST(M[2])})`
 );
 
-const wrapIf = (condition, string) => condition ? `(${string})` : string;
+const fromString = source => _parse(source.replace(/\\/g, 'λ').split(''), []);
 const toString = fold(
   M => toName(M[1]),
   M => `λ${toName(M[1])}.${toString(M[2])}`,
-  M => wrapIf(isLam(M[1]), toString(M[1])) + wrapIf(!isVar(M[2]), toString(M[2]))
+  M => _wrapIf(isLam(M[1]), toString(M[1])) + _wrapIf(!isVar(M[2]), toString(M[2]))
 );
-
-const clone = term => fromString(toString(term));
 
 const redexes = fold(
   M => [],
@@ -81,7 +63,7 @@ const redexes = fold(
 );
 
 const replace = fold(
-  (M, x, N) => x === M[1] ? N : x < M[1] ? Var(M[1] - 1) : M,
+  (M, x, N) => x === M[1] ? copy(N) : x < M[1] ? Var(M[1] - 1) : M,
   (M, x, N) => x === M[1] ? M : Lam(x < M[1] ? M[1] - 1 : M[1], replace(M[2], x, N)),
   (M, x, N) => App(replace(M[1], x, N), replace(M[2], x, N))
 );
@@ -98,63 +80,73 @@ const beta = fold(
   (M, N) => M === N ? replace(M[1][2], M[1][1], M[2]) : App(beta(M[1], N), beta(M[2], N))
 );
 
-const betaGraph = (term, size = 10) => {
-  let stop = false;
+const betaGraph = (term, size = 25) => {
   const nodes = [term];
-  const graph = {};
+  const found = new Set();
+  const graph = new Map();
 
   while (nodes.length && size-- > 0) {
     const term = nodes.shift();
-    const text = toString(term);
-    if (graph[text]) break;
-
-    graph[text] = redexes(term).map(redex => beta(term, redex));
-    graph[text].forEach(term => nodes.push(term));
+    found.add(toString(term));
+    graph.set(term, redexes(term).map(redex => [beta(term, redex), redex]));
+    graph.get(term).forEach(pair => {
+      if (!found.has(toString(pair[0])))
+        nodes.push(pair[0]);
+    });
   }
 
   return graph;
 };
 
-const betaGraphToDot = graph => `digraph G {${Object.entries(graph).map(([text, terms]) => terms.map(term => `"${text}" -> "${toString(term)}"`)).reduce((a, b) => (a.push(...b), a), ['']).join('\n  ')}\n}`;
+if (typeof window !== 'undefined') {
+  const D3 = require('d3');
+  const dagreD3 = require('dagre-d3');
 
-const d3 = require('d3');
-const dd3 = require('dagre-d3');
-const dot = require('graphlib-dot');
+  const render = dagreD3.render();
+  const zoom = D3.zoom().on('zoom', () => g.attr('transform', D3.event.transform));
+  const body = D3.select('body');
+  const input = body.append('input');
+  const svg = body.append('svg');
+  const g = svg.append('g');
+  svg.call(zoom);
 
-const render = dd3.render();
-const input = d3.select('body').append('input');
-const zoom = d3.zoom().on('zoom', () => tag.attr('transform', d3.event.transform));
-const svg = d3.select('body').append('svg');
-const tag = svg.append('g');
-svg.call(zoom);
+  input.on('input', update);
+  update.call({value: '(λx.xx)(λx.x(λy.y)xx)'});
 
-input.on('input', update);
-update.call({value: '(λx.xx)(λx.(λy.y)xx)'});
+  function update() {
+    try {
+      input.attr('value', this.value);
 
-function update() {
-  try {
-    input.attr('value', this.value);
-    const term = fromString(this.value);
-    const text = betaGraphToDot(betaGraph(term));
-    const node = dot.read(text);
-    render(tag, node);
+      const graph = new dagreD3.graphlib.Graph().setGraph({});
+      for (const [termA, pairs] of betaGraph(fromString(this.value))) {
+        const textA = toString(termA);
+        graph.setNode(textA, {label: textA});
 
-    const graph = node.graph();
-    const scale = 0.8 * Math.min(
-      window.innerWidth / graph.width,
-      window.innerHeight / graph.height
-    );
+        for (const [termB, redex] of pairs) {
+          const textB = toString(termB);
+          graph.setNode(textB, {label: textB});
+          graph.setEdge(textA, textB, {label: ''});
+        }
+      }
 
-    svg.call(
-      zoom.transform,
-      d3.zoomIdentity
-        .translate(
-          (window.innerWidth - graph.width * scale) / 2,
-          (window.innerHeight - graph.height * scale) / 2
-        )
-        .scale(scale)
-    );
-  } catch (error) {
-    console.error(error);
+      render(g, graph);
+
+      const scale = 0.8 * Math.min(
+        window.innerWidth / graph.graph().width,
+        window.innerHeight / graph.graph().height
+      );
+
+      svg.call(
+        zoom.transform,
+        D3.zoomIdentity
+          .translate(
+            (window.innerWidth - graph.graph().width * scale) / 2,
+            (window.innerHeight - graph.graph().height * scale) / 2
+          )
+          .scale(scale)
+      );
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
